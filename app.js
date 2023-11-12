188c08cc6777da86ba8621932f8eefeeaafd3305
kartik bisht <103896578+bkartik01@users.noreply.github.com>
2023-11-13 03:29:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,6 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
